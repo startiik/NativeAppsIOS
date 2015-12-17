0b51dcc7e230636d48dc50aab31a35f3f2b7ce7e
startiik <delanoyedean@gmail.com>
2015-12-17 14:27:36 +0100

    first commit

---
+++ KoffiehuisOntbijtBesteller/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  KoffiehuisOntbijtBesteller
+//
+//  Created by Dean Delanoye on 18/11/15.
+//  Copyright © 2015 dean delanoye. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
